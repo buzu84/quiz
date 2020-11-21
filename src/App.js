27d5a783933d1e3933a676b36e703b88b5120096
Magdalena Buzikiewicz <buzu@op.pl>
2020-11-21 11:22:32 +0100

    Cleaning and preparing environment to work.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Quiz from './components/Quiz'
+import './styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello! Quiz time!
-        </p>
-        
-      </header>
-    </div>
+    <Quiz />
   );
 }
 
---
+++ src/components/Quiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Quiz = () => {
+  return (
+    <div>
+      <p>
+        Hello! Quiz time!
+      </p>
+    </div>
+  );
+}
+
+export default Quiz;
